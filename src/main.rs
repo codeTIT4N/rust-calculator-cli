27e17b804fc878cefe8a7dad04b5505bc144df32
Lokesh Kumar Nalot <lokeshkumar@M1-Mac.local>
2021-12-11 14:23:36 +0530

    Handling Environment Arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args(); //note: here we are declring args as a mutable variable
+
+    let first = args.nth(1).unwrap(); //this will give the first variables value
+    println!("{:?}", first);
 }
