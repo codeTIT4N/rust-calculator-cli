98822c197b63eaa37d855047928765315183adfe
Lokesh Kumar Nalot <lokeshkumar@M1-Mac.local>
2021-12-11 14:40:00 +0530

    minor fix

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::env::{args, Args};
 fn main() {
     let mut args: Args = args(); //note: here we are declring args as a mutable variable
 
-    let first = args.nth(1).unwrap(); //this will give the first variables value
+    let first = args.nth(1).unwrap();
     println!("{:?}", first);
 }
